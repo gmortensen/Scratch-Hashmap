0494ef4105a061226b0a528c492d5958cd04263a
gmortensen <mortenga@oregonstate.edu>
2022-05-29 16:30:13 -0600

    HashMap Implementation with separate chaining remove() and contains_key() methods functioning

--- hash_map_sc.py
+++ hash_map_sc.py
@@ -138,6 +138,7 @@ class HashMap:
         index = self._hash_function(key) % self.get_capacity()
         if self._buckets[index]._head is not None:
             self._buckets[index].remove(key)
+            self._size -= 1
 
 
     def get_keys(self) -> DynamicArray:
